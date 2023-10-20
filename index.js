ed98e32bfd0183262bbd331f4d6c6dd2bf471a07
Juttupan <s6401072510036@email.kmutnb.ac.th>
2023-10-21 02:49:34 +0700

    resume

---
+++ index.js
@@ -0,0 +1,16 @@
+function showmessage(){
+    let msg = "เราจะติดต่อคุณกลับไป";
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phonenumber").value;
+    let email = document.getElementById("Email").value;
+    let comment = document.getElementById("mytext").value;
+    alert(msg);
+    console.log("NAME");
+    console.log(name);
+    console.log("phone");
+    console.log(phone);
+    console.log("Email");
+    console.log(email);
+    console.log("comment");
+    console.log(comment);
+}
